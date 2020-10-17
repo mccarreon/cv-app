c1ddc023f3ed601d9178b09ef430b4be34772444
Matt Carreon <mattcarryon@gmail.com>
2020-10-16 22:20:10 -0700

    Updates

---
+++ precache-manifest.89a88bbf62e6cafcc61260e6ca29a450.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4162afa8c8e5e3230d6ecee0803f9295",
+    "url": "/cv-app/index.html"
+  },
+  {
+    "revision": "e51884b75c343ed30398",
+    "url": "/cv-app/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "6159eddc82b463bb9dc5",
+    "url": "/cv-app/static/js/2.1ecb0855.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/cv-app/static/js/2.1ecb0855.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e51884b75c343ed30398",
+    "url": "/cv-app/static/js/main.af410d1e.chunk.js"
+  },
+  {
+    "revision": "2d430f8632af5e6dbd4f",
+    "url": "/cv-app/static/js/runtime-main.cdd4235a.js"
+  }
+]);
